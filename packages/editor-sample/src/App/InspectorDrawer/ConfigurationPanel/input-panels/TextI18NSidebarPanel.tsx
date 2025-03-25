1dc5e672ca6db44eecc9c56899ffaed541b75e61
lampersky <lampersky@gmail.com>
2025-03-25 13:44:38 +0100

    i18n

---
+++ packages/editor-sample/src/App/InspectorDrawer/ConfigurationPanel/input-panels/TextI18NSidebarPanel.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import { TextI18NProps, TextI18NPropsSchema } from '@lampersky/block-texti18n';
+
+import BaseSidebarPanel from './helpers/BaseSidebarPanel';
+import BooleanInput from './helpers/inputs/BooleanInput';
+import TextInput from './helpers/inputs/TextInput';
+import MultiStylePropertyPanel from './helpers/style-inputs/MultiStylePropertyPanel';
+
+type TextI18NSidebarPanelProps = {
+  data: TextI18NProps;
+  setData: (v: TextI18NProps) => void;
+};
+export default function TextSidebarPanel({ data, setData }: TextI18NSidebarPanelProps) {
+  const [, setErrors] = useState<Zod.ZodError | null>(null);
+
+  const updateData = (d: unknown) => {
+    const res = TextI18NPropsSchema.safeParse(d);
+    if (res.success) {
+      setData(res.data);
+      setErrors(null);
+    } else {
+      setErrors(res.error);
+    }
+  };
+
+  return (
+    <BaseSidebarPanel title="Text block">
+      <TextInput
+        label="Content"
+        rows={5}
+        defaultValue={data.props?.text ?? ''}
+        onChange={(text) => updateData({ ...data, props: { ...data.props, text } })}
+      />
+      <BooleanInput
+        label="Markdown"
+        defaultValue={data.props?.markdown ?? false}
+        onChange={(markdown) => updateData({ ...data, props: { ...data.props, markdown } })}
+      />
+
+      <MultiStylePropertyPanel
+        names={['color', 'backgroundColor', 'fontFamily', 'fontSize', 'fontWeight', 'textAlign', 'padding']}
+        value={data.style}
+        onChange={(style) => updateData({ ...data, style })}
+      />
+    </BaseSidebarPanel>
+  );
+}
