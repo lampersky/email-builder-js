96af27065dbcea0574d4cefdf25dbecb6ddf576a
lampersky <lampersky@gmail.com>
2025-03-27 18:09:55 +0100

    test

---
+++ packages/editor-sample-web-component/src/emailbuilder.component.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from '@usewaypoint/editor-sample';
+import theme from './theme';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+class MyWebComponent extends HTMLElement {
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: "open" });
+
+    const mountPoint = document.createElement("div");
+    shadowRoot.appendChild(mountPoint);
+
+    const emotionRoot = document.createElement("style");
+    shadowRoot.appendChild(emotionRoot);
+
+    const shadowCache = createCache({
+      key: 'css',
+      prepend: true,
+      container: shadowRoot,
+    });
+
+    const th = createTheme(theme, {
+      components: {
+        MuiPopover: {
+          defaultProps: {
+            container: mountPoint,
+          },
+        },
+        MuiPopper: {
+          defaultProps: {
+            container: mountPoint,
+          },
+        },
+        MuiModal: {
+          defaultProps: {
+            container: mountPoint,
+          },
+        },
+      },
+    });
+
+    const root = ReactDOM.createRoot(mountPoint);
+    root.render(
+      <React.StrictMode>
+        <CacheProvider value={shadowCache}>
+          <ThemeProvider theme={th}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+        </CacheProvider>
+      </React.StrictMode>
+    );
+  }
+}
+
+customElements.define("my-web-component", MyWebComponent);
---
+++ packages/editor-sample-web-component/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react-swc';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/email-builder-js/',
+// });
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+      lib: {
+        entry: 'src/emailbuilder.component.jsx',
+        name: 'MyWebComponent',
+        fileName: (format) => `my-web-component.${format}.js`,
+        formats: ['iife'],
+      },
+      rollupOptions: {
+        external: [
+          'react', 'react-dom',
+          //'@emotion/react', '@emotion/styled', '@mui/material',
+          ],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            // '@mui/material': 'MaterialUI',
+            // '@emotion/react': 'emotionReact',
+            // '@emotion/styled': 'emotionStyled',
+          },
+        },
+      },
+    },
+    define: {
+      'process.env': process.env
+    }
+  }
+);
