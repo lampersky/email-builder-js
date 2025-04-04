d276511045c65336c2c5f199199560cb39d3778e
lampersky <lampersky@gmail.com>
2025-04-04 14:46:21 +0200

    test

--- packages/editor-sample/vite.config.ts
+++ packages/editor-sample/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-
+import copy from 'rollup-plugin-copy';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig(({ mode }) => {
@@ -9,11 +9,12 @@ export default defineConfig(({ mode }) => {
         plugins: [react()],
         base: '/email-builder-js/',
         build: {
+          outDir: 'distLib',
           lib: {
             entry: 'src/emailbuilder.component.jsx',
             name: 'EmailBuilderJsComponent',
             fileName: (format) => `email-builder-js-component.${format}.js`,
-            formats: ['iife'],
+            formats: ['iife']
           },
           rollupOptions: {
             external: [
@@ -32,9 +33,19 @@ export default defineConfig(({ mode }) => {
           'process.env': process.env
         }
       }
+    case 'site':
     default:
       return {
-        plugins: [react()],
+        plugins: [
+          react(),
+          copy({
+            targets: [
+              { src: 'distLib/email-builder-js-component.iife.js', dest: 'dist' },
+            ],
+            hook: 'writeBundle',
+            verbose: true, 
+          }),
+        ],
         base: '/email-builder-js/'
       }
   }
